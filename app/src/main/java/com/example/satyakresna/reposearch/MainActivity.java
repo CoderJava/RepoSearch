c1ff0daa61a4de479e0594f9908a579892086731
Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2017-07-02 11:30:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/satyakresna/reposearch/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.satyakresna.reposearch;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListFragment mListFragment;
+    ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            showListFragment(new ArrayList<Repository>());
+        }
+    }
+
+    private void showListFragment(ArrayList<Repository> repositories) {
+        mListFragment = ListFragment.newInstance(repositories);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mListFragment).
+                commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+
+    }
+
+    private void startDownload() {
+    }
+}
